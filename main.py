f490fcad95b8147e7084baaf3397b4348134f97b
oscar-99 <oscarsmee99@gmail.com>
2019-01-16 10:08:17 +1000

    Updates

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ from boardui import BoardGUI
 from strategy import StrategyMk1
 import utilities
 
-utilities.puzzle_import(50)
+
 sum = 0
-for i in range(1, 50):
-    puzzle_number = i
-    board = utilities.generate_puzzle_matrix(puzzle_number)
-    game = SudokuBoard(board, puzzle_number)
-    strat = StrategyMk1(game)
-    strat.solver()
-    sum += strat.game.get_top_sum()
+
+puzzle_number = 1
+board = utilities.generate_puzzle_matrix(puzzle_number)
+game = SudokuBoard(board, puzzle_number)
+strat = StrategyMk1(game)
+strat.solver()
+sum += strat.game.get_top_sum()
+
+
--- strategy.py
+++ strategy.py
@@ -11,54 +11,6 @@ class StrategyMk1(object):
         self.solution_matrix = []
 
 
-    def get_row_numbers(self, row):
-        """
-        Method for getting missing numbers along a row.
-
-        Parameters:
-            row (int): The row to get missing numbers from.
-
-        Returns:
-            (list): List of numbers missing. 
-        """
-        return [i for i in range(1,10) if i not in self.game.board[row]]
-
-
-    def get_column_numbers(self, column):
-        """
-        Method for getting missing numbers along a column.
-
-        Parameters:
-            column (int): The column to get missing numbers from.
-
-        Returns:
-            (list): List of numbers missing. 
-        """
-        column_list = []
-        for i in range(9):
-            column_list.append(self.game.board[i][column])
-
-        return [i for i in range(1,10) if i not in column_list]
-
-
-    def get_square_numbers(self, square):
-        """
-        Method for getting missing numbers in a 3x3 square in following number scheme:
-            1 2 3
-            4 5 6
-            7 8 9
-
-        Parameters:
-            square (int): The square to get missing numbers from.
-
-        Returns:
-            (list): List of missing numbers 
-        """
-        square = self.game.get_sub_squares()[square]
-        square = square.flatten().tolist()
-        return [i for i in range(1, 10) if i not in square]
-
-    
     def search_grid(self):
         """
         Method that searches the grid by square, by row and then by column to find potential entries. 
@@ -68,11 +20,11 @@ class StrategyMk1(object):
         self.solution_matrix = []
         for i in range(9):
             solution_row = []
-            row_missing = self.get_row_numbers(i)
+            row_missing = self.game.board.get_row_numbers(i)
             for j in range(9):
                 if self.game.board[i][j] == 0:
-                    column_missing = self.get_column_numbers(j)
-                    square_missing = self.get_square_numbers(utilities.coords_to_square(i,j))
+                    column_missing = self.game.get_column_numbers(j)
+                    square_missing = self.game.get_square_numbers(utilities.coords_to_square(i,j))
                     position_missing = [i for i in range(1, 10) if i in row_missing and i in column_missing and i in square_missing]
                     solution_row.append(position_missing)
                 else: solution_row.append([])
@@ -106,7 +58,7 @@ class StrategyMk1(object):
 
         while self.step():
             if self.game.check_board_full():
-                if self.game.check_solution():
+                if self.game.check_board():
                     print("Solved: \n")
                     print(self.game.board)
                     sum = self.game.get_top_sum()
@@ -117,7 +69,12 @@ class StrategyMk1(object):
                 break
 
 
-        
+
+class StrategyMk2(object):
+    """
+    This strategy aims to implent a strategy that can deal with non logical solutions. 
+    """
+    pass      
 
 
         
